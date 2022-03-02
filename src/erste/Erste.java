9d0e1dc8feec465c81a384f377fe3355ce95fbb3
s51633 <rnawaqil@gmail.com>
2022-03-02 09:46:31 +0100

    added erste file

--- src/erste/Erste.java
+++ src/erste/Erste.java
@@ -4,6 +4,7 @@ public class Erste {
     public static void main(String[] args) {
         //write your code her
         System.out.println("hello world" );
-        
+        System.out.println("name");
+
     }
 }
